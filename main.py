1d964b3264ae627ce13a3849e52e19eb4683fef1
Muiz_Git_SourceTree <muiznaveedrana@gmail.com>
2024-08-14 16:23:50 +0100

    commit

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ app = Flask("Website")
 def home():
     return render_template("tutorial.html")
 
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
 app.run(debug=True)
