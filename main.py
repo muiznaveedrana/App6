8752b58400402aae93fad35ea5b86e42505b95a4
Muiz_Git_SourceTree <muiznaveedrana@gmail.com>
2024-08-16 18:21:13 +0100

    Make FR

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 
 app = Flask(__name__)
@@ -9,9 +10,15 @@ def home():
 
 @app.route("/api/<station>/<date>")
 def about(station, date):
-    temprature = "23"
+    filename = "Data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates= ['    DATE'])
+
+    temprature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+
+    
     return {"station": station,
             "date": date,
             "temperature": temprature}
+
 if __name__ == "__main__":
     app.run(debug=True)
